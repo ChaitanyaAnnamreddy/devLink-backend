3b055b1edaa1bdd3cbabd3a35ef00b931d2f94f5
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-28 22:19:31 +0530

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  console.log('🚀 Connecting to MongoDB...')
-  await mongoose.connect(
-    'mongodb+srv://dev-link:bLwhydQN1mSD0x2I@cluster0.kmbym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  )
+  try {
+    const conn = await mongoose.connect(
+      'mongodb+srv://dev-link:bLwhydQN1mSD0x2I@cluster0.kmbym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    )
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Database connection failed: ${error.message}`)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
