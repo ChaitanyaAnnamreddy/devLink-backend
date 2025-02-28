d12da507491e0fa132028b5e8505ddfb7a201cb3
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-28 16:29:55 +0530

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  console.log(process.env.DB_CONNECTION_SECRET)
+  console.log('🚀 Connecting to MongoDB...')
   await mongoose.connect(
     'mongodb+srv://dev-link:rr4voCFiPmurcTYj@cluster0.kmbym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   )
